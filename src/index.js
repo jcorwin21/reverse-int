8c4b33b1ae2977227f60c957c645db09165cb27f
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-07-06 19:35:59 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    const str = '' + n;
+    const arr = [...str];
+    const rev = arr.reverse();
+    const joined = rev.join('');
+    return +joined;
 }
